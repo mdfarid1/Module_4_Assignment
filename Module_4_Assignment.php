1e9771fbde5c2c74eaebf4232fbca77becb2e7ae
mdfarid1 <mfaridhossain24@gmail.com>
2023-10-04 20:51:48 +0600

    done

--- Module_4_Assignment.php
+++ Module_4_Assignment.php
@@ -1,10 +1,10 @@
 <?php
     class Product {
-        private  $id;
-        private  $name;
-        private  $price;
+        public  $id;
+        public  $name;
+        public  $price;
 
-        public function __construct($id,$name,$price) {
+        public function __construct(int $id, string $name,float $price) {
             $this->id=$id;
             $this->name=$name;
             $this->price=$price;
